c1a82d8c935295282e10f322d025637a70f79d43
aajay007 <40169124+aajay007@users.noreply.github.com>
2021-10-25 12:10:21 +0530

    First Commit

---
+++ Thinkbridge/WebAPIApplication/Controllers/ProductController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using WebAPIApplication.Models;
+using WebAPIApplication.Service;
+
+namespace WebAPIApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        IProductService productService;
+        public ProductController(IProductService _ProductService)
+        {
+            productService = _ProductService;
+        }
+
+
+        [HttpGet("/api/Product")]
+        public async Task<IActionResult> GetProduct()
+        {
+            try
+            {
+                var products = await productService.GetProduct();
+                if (products == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(products);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+
+        }
+
+
+        [HttpGet("/api/Product/{id}")]
+        public async Task<IActionResult> GetProduct(int Id)
+        {
+            try
+            {
+                var post = await productService.GetProduct(Id);
+
+                if (post == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(post);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+
+        [HttpPost("/api/Product")]
+        public async Task<IActionResult> AddProduct([FromBody] Product Product)
+        {
+            try
+            {
+                var postId = await productService.AddProduct(Product);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+
+        }
+
+
+        [HttpPut("/api/Product")]
+        public async Task<IActionResult> PutProduct([FromBody] Product Product)
+        {
+            try
+            {
+                await productService.PutProduct(Product);
+
+                return Ok();
+            }
+            catch (Exception)
+
+            {
+                return BadRequest();
+            }
+
+        }
+
+        [HttpPatch("/api/Product")]
+        public async Task<IActionResult> PatchProduct([FromBody] Product Product)
+        {
+
+            try
+            {
+                await productService.PatchProduct(Product);
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+
+        }
+
+
+        [HttpDelete("/api/Product/{id}")]
+        public async Task<IActionResult> DeleteProduct(int Id)
+        {
+            int result = 0;
+            try
+            {
+                result = await productService.DeleteProduct(Id);
+                if (result == 0)
+                {
+                    return NotFound();
+                }
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ Thinkbridge/WebAPIApplication/Models/ThinkbridgeDBContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIApplication.Models
+{
+    public partial class ThinkbridgeDBContext : DbContext
+    {
+        public ThinkbridgeDBContext()
+        {
+        }
+
+        public ThinkbridgeDBContext(DbContextOptions<ThinkbridgeDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> ProductDetailsTable { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Description)
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+
+    
+}
